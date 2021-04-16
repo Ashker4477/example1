77039b8786bf2627c0a88ea87ef2cd097d6cb3f5
muhammed Ashker <muhammedashker3@gmail.com>
2021-04-16 08:00:21 +0530

    check box array and table ready

--- front-end/src/Screens/HomeScreen.js
+++ front-end/src/Screens/HomeScreen.js
@@ -1,14 +1,51 @@
-import React, { useEffect, useState } from "react";
+import React, { useCallback, useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useHistory } from "react-router";
 import { signout } from "../Actions/userActions";
 
 function HomeScreen() {
   const [expanded, setExpanded] = useState(false);
-  const [one, setOne] = useState(false);
-  const [two, setTwo] = useState(false);
-  const [three, setThree] = useState(false);
   const [item, setItem] = useState([]);
+  const [values, setValues] = useState({
+    one: {
+      checked: false,
+      value: "",
+    },
+    two: {
+      checked: false,
+      value: "",
+    },
+    three: {
+      checked: false,
+      value: "",
+    },
+  });
+  const history = useHistory();
+  const dispatch = useDispatch();
+
+  const userSignin = useSelector((state) => state.userSignin);
+  const { userInfo, loading, error } = userSignin;
+
+  useEffect(() => {
+    if (!userInfo) {
+      history.push("/signin");
+    }
+  }, [history, userInfo]);
+
+  const signoutHandler = (e) => {
+    e.preventDefault();
+    console.log("signout");
+    dispatch(signout);
+  };
+
+  const handleChange = useCallback((e) => {
+    setValues((prevVal) => {
+      const newVal = { ...prevVal };
+      newVal[e.target.name].checked = e.target.checked;
+      newVal[e.target.name].value = e.target.value;
+      return newVal;
+    });
+  }, []);
 
   const showCheckboxes = (e) => {
     e.preventDefault();
@@ -21,86 +58,86 @@ function HomeScreen() {
       setExpanded(false);
     }
   };
+
   const submitHandler = (e) => {
     e.preventDefault();
-    if (one && !two && !three) {
-      const pack = "value added for 1";
-      setItem([...item, { pack }]);
-    }
-    if (!one && two && !three) {
-      const pack = "value added for 2";
-      setItem([...item, { pack }]);
-    }
-    if (!one && !two && three) {
-      const pack = "value added for 3";
-      setItem([...item, { pack }]);
-    }
-    if (one && two && three) {
-      const pack1 = "value added for 1";
-      const pack2 = "value added for 2";
-      const pack3 = "value added for 3";
-      const pack = { pack1, pack2, pack3 };
-      setItem([...item, {pack}]);
-    }
+    const obArray = Object.entries(values)
+      .filter((ob) => ob[1].checked)
+      .map((ob) => ob[1].value);
+    setItem([...item, obArray]);
     console.log(item);
   };
+
   return (
-    <div className="home">
-      <form onSubmit={submitHandler}>
-        <div className="multiselect">
-          <div className="selectBox" onClick={showCheckboxes}>
-            <select>
-              <option>Select an option</option>
-            </select>
-            <div className="overSelect"></div>
+    <div className="center">
+      <div className="row">
+        <form onSubmit={submitHandler}>
+          <div className="multiselect">
+            <div className="selectBox" onClick={showCheckboxes}>
+              <select>
+                <option>Select an option</option>
+              </select>
+              <div className="overSelect"></div>
+            </div>
+            <div id="checkboxes">
+              <label htmlFor="one">
+                <input
+                  type="checkbox"
+                  name="one"
+                  value="value added for 1 "
+                  checked={values.one.checked}
+                  onChange={handleChange}
+                />
+                value added for 1
+              </label>
+              <label htmlFor="two">
+                <input
+                  type="checkbox"
+                  name="two"
+                  value="value added for 2 "
+                  checked={values.two.checked}
+                  onChange={handleChange}
+                />
+                value added for 2
+              </label>
+              <label htmlFor="three">
+                <input
+                  type="checkbox"
+                  name="three"
+                  value="value added for 3 "
+                  checked={values.three.checked}
+                  onChange={handleChange}
+                />
+                value added for 3
+              </label>
+            </div>
           </div>
-          <div id="checkboxes">
-            <label htmlFor="one">
-              <input
-                type="checkbox"
-                id="one"
-                value={one}
-                onChange={(e) => setOne(e.target.checked)}
-              />
-              value added for 1
-            </label>
-            <label htmlFor="two">
-              <input
-                type="checkbox"
-                id="two"
-                value={two}
-                onChange={(e) => setTwo(e.target.checked)}
-              />
-              value added for 2
-            </label>
-            <label htmlFor="three">
-              <input
-                type="checkbox"
-                id="three"
-                value={three}
-                onChange={(e) => setThree(e.target.checked)}
-              />
-              value added for 3
-            </label>
+          <div className="primary">
+            <button type="submit">Save</button>
           </div>
-        </div>
-        <div className="primary">
-          <button type="submit">Save</button>
-        </div>
-      </form>
-      
-        <tr>
-            <th>Sl.No:</th>
-            <th>Description</th>
-          </tr>(
-           { item.pack ? item.map((i, v) => {
+        </form>
+        {item ? (
+          <table>
+            <tbody>
               <tr>
-                <td>{i + 1}</td>
-                <td>{v.pack}</td>
-              </tr>;
-            }): null}
-          )
-        
+                <th>Sl.No</th>
+                <th>Description</th>
+              </tr>
+              {item.map((ite, index) => {
+                return (
+                  <tr key={index}>
+                    <td>{index + 1}</td>
+                    <td>{ite}</td>
+                  </tr>
+                );
+              })}
+            </tbody>
+          </table>
+        ) : null}
+      </div>
+      <div>
+        <button onClick={signoutHandler}>Signout</button>
+      </div>
     </div>
   );
 }
